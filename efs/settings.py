8ef2d3d320d9610ac41031b2722c953712f63d67
Kevin Kwak <kkwak@unomaha.edu>
2022-02-20 08:45:44 -0600

    port fix

--- efs/settings.py
+++ efs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v@=+_5ypm^+5q%-y+talel-2!h288#8*ha-8x)vama^3(!(=he'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -80,7 +80,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'efs',
         'USER': 'instructor',
-        'PASSWORD': 'maverick1a'
+        'PASSWORD': 'maverick1a',
+        'PORT': '5432'
     }
 }
 
