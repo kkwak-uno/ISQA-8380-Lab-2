3360213c8558604f224d810e1097414903856941
Kevin Kwak <kkwak@unomaha.edu>
2022-02-20 10:10:11 -0600

    engine fix

--- efs/settings.py
+++ efs/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'efs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'efs',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': 'instructor',
         'PASSWORD': 'maverick1a',
         'PORT': '5432'
